d4021c83d45239dca7acb3556e70fdc2eb5e365e
Guboshlyop <ipsimonovpi@gmail.com>
2024-11-17 17:16:17 +0800

    Modified calculating part. Still has not perfect graphics part

--- FigureCalculator.py
+++ FigureCalculator.py
@@ -1,97 +1,73 @@
-#Программа не высчитывает пересечения и объединения фигур
-#Требуются библиотеки PySimpleGUI и matplotlib
+# Программа не высчитывает пересечения и объединения фигур
+# Требуются библиотеки PySimpleGUI и matplotlib
 counterx = 400
 countery = 0
 import math
 def schyot(x1, y1, x2, y2, x3, y3, x4, y4):
-#Любую фигуру можно представить в виде прямоугольников и треугольников
-#Выделим основной прямоугольник
+# Любую фигуру можно представить в виде прямоугольников и треугольников
+# Выделим основной прямоугольник
     x11 = min(x1, x2, x3, x4)
     x12 = max(x1, x2, x3, x4)
     y11 = min(y1, y2, y3, y4)
     y12 = max(y1, y2, y3, y4)
-#Найдём его площадь
+# Найдём его площадь
     S = abs((x12-x11)*(y12-y11))
-#При каждой из сторон фигуры выделим прямоугольные треугольники
-    h1 = abs(y2 - y1)
-    a1 = abs(x2 - x1)
+# При каждой из сторон фигуры выделим прямоугольные треугольники
+    h1 = abs(y1 - y2)
+    a1 = abs(x1 - x2)
     h2 = abs(y2 - y3)
-    a2 = abs(x3 - x2)
+    a2 = abs(x2 - x3)
     h3 = abs(y3 - y4)
     a3 = abs(x3 - x4)
-    h4 = abs(y1 - y4)
+    h4 = abs(y4 - y1)
     a4 = abs(x4 - x1)
-#Следом найдём их площадь
-    S1 = (1/2)*(a1*h1)
-    S2 = (1/2)*(a2*h2)
-    S3 = (1/2)*(a3*h3)
-    S4 = (1/2)*(a4*h4)
-#Выделяем и считаем частные случаи
+# Следом найдём их площадь
+    S -= (1/2)*(a1*h1)
+    S -= (1/2)*(a2*h2)
+    S -= (1/2)*(a3*h3)
+    S -= (1/2)*(a4*h4)
+# Выделяем и считаем частные случаи
     if y1 < y2 < y3 and x1 < x2 < x3:
         z1 = x1 - x2
         w1 = y3 - y2
-        S1d = abs(z1*w1)
-    else:
-        S1d = 0
+        S -= abs(z1*w1)
+    if y4 < y3 < y2 and x2 < x3 < x4:
+        z1 = x12 - x3
+        w1 = y12 - y3
+        S -= abs(z1*w1)
+    if y1 < y4 < y3 and x1 < x4 < x3:
+        z1 = x4 - x12
+        w1 = y11 - y4
+        S -= abs(z1*w1)
     if y2 < y3 < y1 and x1 < x2 < x3:
         z2 = x3 - x2
         w2 = y3 - y1
-        S2d = abs(z2*w2)
-    else:
-        S2d = 0
+        S -= abs(z2*w2)
     if x3 < x4 < x2 and y2 > y3 > y4:
         z3 = x2 - x4
         w3 = y3 - y4
-        S3d = abs(z3*w3)
-    else:
-        S3d = 0
+        S -= abs(z3*w3)
     if y4 > y1 > y3 and x1 < x4 < x3:
         z4 = x4 - x1
         w4 = y1 - y11
-        S4d = abs(z4*w4)
-    else:
-        S4d = 0
+        S -= abs(z4*w4)
     if y2 < y1 < y3 and x1 < x2 < x3:
         z2s = x2 - x1
         w2s = y3 - y1
-        S2s = abs(z2s*w2s)
-    else:
-        S2s = 0
+        S -= abs(z2s*w2s)
     if x3 < x2 < x4 and y2 > y3 > y4:
         z3s = x4 - x2
         w3s = y2 - y3
-        S3s = abs(z3s*w3s)
-    else:
-        S3s = 0
+        S -= abs(z3s*w3s)
     if y4 > y3 > y1 and x1 < x4 < x3:
         z4s = x3 - x4
         w4s = y3 - y1
-        S4s = abs(z4s*w4s)
-    else:
-        S4s = 0
-    if y1 < y12 > y2:
-        s1 = abs((y12 - max(y2, y1))*(x1 - x2))
-    else:
-        s1 = 0
+        S -= abs(z4s*w4s)
     if y2 < y12 > y3:
-        s2 = abs((y12 - max(y2, y3))*(x3 - x2))
-        if y2 > y1:
-            s2 += abs((x11 - x2)*(y12 - y3))
-    else:
-        s2 = 0
-    if x2 < x12 > x3:
-        s3 = abs((x12 - max(x3, x2))*(y2 - y3))
-    else:
-        s3 = 0
-    if y3 > y11 < y4:
-        s4 = abs((min(y3, y4) - y11)*(x3 - x4))
-    else:
-        s4 = 0
+        S -= abs((y12 - max(y2, y3))*(x3 - x2))
     if y4 > y11 < y1:
-        s5 = abs((min(y4, y1) - y11)*(x4 - x1))
-    else:
-        s5 = 0
-#Находим площадь и периметр искомого четырёхугольника
+        S -= abs((min(y4, y1) - y11)*(x4 - x1))
+#Находим периметр искомого четырёхугольника
     A1 = math.sqrt(a1**2 + h1**2)
     A2 = math.sqrt(a2**2 + h2**2)
     A3 = math.sqrt(a3**2 + h3**2)
@@ -103,7 +79,7 @@ def schyot(x1, y1, x2, y2, x3, y3, x4, y4):
     if x3 == x2 and y3 == y2:
         if x1 == x4 and y1 == y4:
             Pfigure /= 2
-    Sfigure = abs(S - S1 - S2 - S3 - S4 - S1d - S2d - S3d - S4d - S2s - S3s - S4s - s2 - s3 - s4 - s5)
+    Sfigure = S
     return Sfigure, Pfigure
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
