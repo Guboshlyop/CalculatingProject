5a7b42f7630a95edeac15393b688be291f9b8edb
Guboshlyop <ipsimonovpi@gmail.com>
2024-12-01 16:03:50 +0800

    Improved graphics part and calculating part.

--- FigureCalculator.py
+++ FigureCalculator.py
@@ -2,6 +2,9 @@
 # Требуются библиотеки PySimpleGUI и matplotlib
 counterx = 400
 countery = 0
+i = 4
+j = 2
+h = 1
 import math
 def schyot(x1, y1, x2, y2, x3, y3, x4, y4):
 # Любую фигуру можно представить в виде прямоугольников и треугольников
@@ -32,8 +35,8 @@ def schyot(x1, y1, x2, y2, x3, y3, x4, y4):
         w1 = y3 - y2
         S -= abs(z1*w1)
     if y4 < y3 < y2 and x2 < x3 < x4:
-        z1 = x12 - x3
-        w1 = y12 - y3
+        z1 = x2 - x3
+        w1 = y2 - y3
         S -= abs(z1*w1)
     if y1 < y4 < y3 and x1 < x4 < x3:
         z1 = x4 - x12
@@ -67,7 +70,15 @@ def schyot(x1, y1, x2, y2, x3, y3, x4, y4):
         S -= abs((y12 - max(y2, y3))*(x3 - x2))
     if y4 > y11 < y1:
         S -= abs((min(y4, y1) - y11)*(x4 - x1))
-#Находим периметр искомого четырёхугольника
+    if y1 < y2 < y3 < y4 and x1 < x2 < x3 < x4:
+        S -= abs((x2 - x1)*(y4 - y3))
+    if y1 < y4 < y3 < y2 and x1 < x4 < x3 < x2:
+        S -= abs((x2 - x3)*(y4 - y1))
+    if y1 > y4 > y3 > y2 and x1 < x4 < x3 < x2:
+        S -= abs((x4 - x1)*(y3 - y2))
+    if y1 > y2 > y3 > y4 and x1 < x2 < x3 < x4:
+        S -= abs((x4 - x3)*(y1 - y2))
+# Находим периметр искомого четырёхугольника
     A1 = math.sqrt(a1**2 + h1**2)
     A2 = math.sqrt(a2**2 + h2**2)
     A3 = math.sqrt(a3**2 + h3**2)
@@ -79,12 +90,12 @@ def schyot(x1, y1, x2, y2, x3, y3, x4, y4):
     if x3 == x2 and y3 == y2:
         if x1 == x4 and y1 == y4:
             Pfigure /= 2
-    Sfigure = S
+    Sfigure = abs(S)
     return Sfigure, Pfigure
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import PySimpleGUI as sg
-import matplotlib
 matplotlib.use('TkAgg')
 sg.theme('DarkAmber')
 fig = matplotlib.figure.Figure(figsize=(4, 3), dpi=100)
@@ -96,9 +107,8 @@ def draw_figure(canvas, figure):
    return tkcanvas
 
 def graphics1():
-#После нажатия на кнопку запуска выволится окно с вводом данных
-    layout1 = [[sg.Text('Пожалуйста, не закрывайте это окно во время работы. Это приведёт к остановке программы.', size=(100, 1), key='-text-', font='Helvetica 16')],
-              [sg.Text('Введите упорядоченные относительные координаты углов (первая вершина левая нижняя, обход по часовой стрелке):', size=(100, 1), key='-text-', font='Helvetica 16')], 
+# После нажатия на кнопку запуска выволится окно с вводом данных
+    layout1 = [[sg.Text('Введите упорядоченные относительные координаты углов (первая вершина левая нижняя, обход по часовой стрелке):', size=(100, 1), key='-text-', font='Helvetica 16')], 
               [sg.Text('x1', size=(15,1), key='-text-', font='Helvetica 16')], [sg.Input(key='-x1-', do_not_clear=True)],
               [sg.Text('y1', size=(15,1), key='-text-', font='Helvetica 16')], [sg.Input(key='-y1-', do_not_clear=True)],
               [sg.Text('x2', size=(15,1), key='-text-', font='Helvetica 16')], [sg.Input(key='-x2-', do_not_clear=True)],
@@ -112,7 +122,7 @@ def graphics1():
     event, values = window1.read()
     
     if event == '-G-':
-#Выносим данные из ячеек ввода
+# Выносим данные из ячеек ввода
         x1 = float(values['-x1-'])
         y1 = float(values['-y1-'])
         x2 = float(values['-x2-'])
@@ -125,7 +135,7 @@ def graphics1():
     return x1, x2, x3, x4, y1, y2, y3, y4
 
 def graphics2(Sfigure, Pfigure, counterx, countery):
-#Выводим результат счёта на экран
+# Выводим результат счёта на экран
     layout3 = [[sg.Text('Площадь новой фигуры равна:', size = (30,1), key = '-text-', font = 'Helvetica 16')],
               [sg.Text(Sfigure, size = (30,1), key = '-text-', font = 'Helvetica 16')],
               [sg.Text('Периметр новой фигуры равен:', size = (30,1), key = '-text-', font = 'Helvetica 16')],
@@ -136,30 +146,31 @@ def graphics2(Sfigure, Pfigure, counterx, countery):
     tkcanvas = draw_figure(window3['-CANVAS-'].TKCanvas, fig)
     event, values = window3.read()
     
-#Создаём кнопку запуска программы
+# Создаём кнопку запуска программы
 layout = [[sg.Text('Функции программы:', size = (30,1), key = '-text-', font = 'Helvetica 16')],
           [sg.Text('Программа расчитана на создание четырёхугольников разных форм и рассчёт их площадей и периметров, однако с её помощью можно создавать много чего ещё:', size = (30,5), key = '-text-', font = 'Helvetica 16')],
-          [sg.Text('1. Чтобы добавить многоугольник, отличный от четырёхугольника, представьте его в виде нескольких четырёхугольников с общей стороной.', size = (30,5), key = '-text-', font = 'Helvetica 16')],
-          [sg.Text('2. Чтобы найти площадь и периметр объединения или пересечения, представьте объединение или пересечение в виде многоугольника и действуйте по аналогии с п. 1. Чтобы найти координаты пересечений фигур, наведите на пересечения фигур курсором мыши на окне с чертежом.', size = (30,9), key = '-text-', font = 'Helvetica 16')],
+          [sg.Text('1. Чтобы создать многоугольник, отличный от четырёхугольника, представьте его в виде нескольких четырёхугольников с общей стороной.', size = (30,5), key = '-text-', font = 'Helvetica 16')],
+          [sg.Text('2. Чтобы найти площадь и периметр объединения или пересечения, представьте объединение или пересечение в виде многоугольника и действуйте по аналогии с п. 1.', size = (30,5), key = '-text-', font = 'Helvetica 16')],
           [sg.Text('3. Чтобы создать треугольник, представьте его в виде четырёхугольника с 1 развёрнутым углом (Углом равным 180 градусам).', size = (30,5), key = '-text-', font = 'Helvetica 16')],
           [sg.Button('Старт программы', enable_events=True, key='-FUNCTION-', font='Helvetica 16')]],
 window = sg.Window('Старт', layout, size=(400,700))
 event, values = window.read()
 while True:
-#Если закрыть окно с этой кнопкой, программа прекратит работу
+# Если закрыть окно с этой кнопкой, программа прекратит работу
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
-#Производим процедуру счёта
+# Производим процедуру счёта
     if event == '-FUNCTION-':
         window.close()
         x1, x2, x3, x4, y1, y2, y3, y4 = graphics1()
         Sfigure, Pfigure = schyot(x1, y1, x2, y2, x3, y3, x4, y4)
-#Создаём визуализацию фигуры
+# Создаём визуализацию фигуры
         x1p = [x1, x2, x3, x4, x1]
         y1p = [y1, y2, y3, y4, y1]
-        plot = plt.plot(x1, y1)
-        fig.add_subplot(111).plot(x1p, y1p)
-#Выводим площадь фигуры и её визуализацию
+        fig.add_subplot(111).plot(x1p, y1p, '-b')
+        x2p = [x1]
+        y2p = [y1]
+# Выводим площадь фигуры и её визуализацию
         layout2 = [[sg.Text('Площадь фигуры равна:', size = (30,1), key = '-text-', font = 'Helvetica 16')],
                   [sg.Text(float(Sfigure), size = (30,1), key = '-text-', font = 'Helvetica 16')],
                   [sg.Text('Периметр фигуры равен:', size = (30,1), key = '-text-', font = 'Helvetica 16')],
@@ -169,19 +180,49 @@ while True:
         window2 = sg.Window('Результат', layout2, size=(400, 500), location=(0, 0), finalize=True)
         tkcanvas = draw_figure(window2['-CANVAS-'].TKCanvas, fig)
         event, values = window2.read()
-#Введение новой фигуры
+# Введение новой фигуры
     if event == '-Z-':
         while True:
+            exc = 0
             x1, x2, x3, x4, y1, y2, y3, y4 = graphics1()
             Sfigure, Pfigure = schyot(x1, y1, x2, y2, x3, y3, x4, y4)
             fig = matplotlib.figure.Figure(figsize=(4, 3), dpi=100)
-            fig.add_subplot(111).plot(x1p, y1p, '--', [x1, x2, x3, x4, x1], [y1, y2, y3, y4, y1])
+            fig.add_subplot(111).plot(x1p, y1p, ':k', x2p, y2p, '-w', [x1, x2, x3, x4, x1], [y1, y2, y3, y4, y1], '-b')
             x1p += [x1, x2, x3, x4, x1]
             y1p += [y1, y2, y3, y4, y1]
             graphics2(Sfigure, Pfigure, counterx, countery)
+            # Изменим местоположение окон, чтобы от части избежать наложения
             counterx += 400
             if counterx == 2000:
                 counterx = 0
                 countery = 500
             if countery == 500 and counterx == 2000:
                 countery = 0
+            # Избавимся от связок между двумя фигурами
+            if x1p[i] == x1p[i + 2] and y1p[i] == y1p[i + 2]:
+                exc += 0
+            else:
+                exc += 1
+                
+            if x1p[j] == x1p[j + 4] and y1p[j] == y1p[j + 4]:
+                exc += 0
+            else:
+                exc += 1
+
+            if x1p[h] == x1p[h + 6] and y1p[h] == y1p[h + 6]:
+                exc += 0
+            else:
+                exc += 1
+
+            if x1p[h] == x1p[i + 5] and y1p[h] == y1p[i + 5]:
+                exc += 0
+            else:
+                exc += 1
+
+            if exc == 4:
+                x2p += [x1]
+                y2p += [y1]
+                
+            i += 5
+            j += 5
+            h += 5
