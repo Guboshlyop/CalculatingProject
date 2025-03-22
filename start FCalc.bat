94a75860b244d229f8969ebea9eee0ef7e3b8c67
Guboshlyop <ipsimonovpi@gmail.com>
2025-03-22 17:32:30 +0800

    Ведётся работа над установочным файлом

--- start FCalc.bat
+++ start FCalc.bat
@@ -1,4 +1,6 @@
 @echo off
-call C:\Users\1\Desktop\CalculatingProject\figure_calc\Scripts\activate.bat
-python FigureCalculator.py
+set BAT_DIR=%~dp0
+cd /d "%BAT_DIR%"
+call "%BAT_DIR%figure_calc\Scripts\activate"
+python "%BAT_DIR%FigureCalculator.py"
 deactivate
